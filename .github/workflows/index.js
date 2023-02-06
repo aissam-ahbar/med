1dff7597b98bccae7be22bddb765021828c12190
Aissam Ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2023-02-06 22:29:29 +0100

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -2,11 +2,15 @@ const fs = require('fs');
 const brotli = require('brotli');
 const request = require('request');
 
-const url = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/medecins/exports/json?lang=fr&timezone=Europe%2FBerlin';
+const url = 'http://example.com/large-file.txt';
 const inputFile = 'large-file.txt';
 const outputFile = 'large-file.txt.br';
 
-const input = request(url);
-const output = fs.createWriteStream(outputFile);
-
-input.pipe(brotli.compressStream()).pipe(output);
+request(url)
+  .pipe(fs.createWriteStream(inputFile))
+  .on('finish', () => {
+    const input = fs.createReadStream(inputFile);
+    const output = fs.createWriteStream(outputFile);
+  
+    input.pipe(brotli.compressStream()).pipe(output);
+  });

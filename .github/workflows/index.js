7ed99cfb68bd0b2f7b46997e96a25ff8299b4940
Aissam Ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2023-02-06 22:31:28 +0100

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const brotli = require('brotli');
 const request = require('request');
 
-const url = 'http://example.com/large-file.txt';
+const url = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/medecins/exports/json?lang=fr&timezone=Europe%2FBerlin';
 const inputFile = 'large-file.txt';
 const outputFile = 'large-file.txt.br';
 
